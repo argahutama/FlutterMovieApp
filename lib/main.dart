3ec3f4acebbe97d3ee6a48b3d9afdc790ebce546
Arga Hutama <argahut@gmail.com>
2023-10-26 20:47:32 +0700

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:common/constants.dart';
 import 'package:common/utils.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/firebase_options.dart';
 import 'package:flutter_movie_app/route/home_page.dart';
 import 'package:flutter_movie_app/route/route.dart';
 
 import '../di/injection.dart' as di;
 
-void main() {
+void main() async {
   di.getDependencies();
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(const MyApp());
 }
 
